b35d0429d8a54f82f77f260429ab52bd6d2e6fe3
vuthaihoc <vuthaihoc.ttud@gmail.com>
2025-08-19 15:51:05 +0700

    hotfix

--- src/Resources/JobsWaitingResource.php
+++ src/Resources/JobsWaitingResource.php
@@ -12,7 +12,7 @@ use Filament\Forms\Components\Toggle;
 use Filament\Resources\Resource;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Moox\Core\Traits\Tabs\TabsInResource;
+use Moox\Core\Traits\Tabs\HasResourceTabs;
 use Moox\Jobs\Models\Job;
 use Moox\Jobs\Resources\JobsWaitingResource\Pages\ListJobsWaiting;
 use Moox\Jobs\Resources\JobsWaitingResource\Widgets\JobsWaitingOverview;
@@ -20,7 +20,7 @@ use Override;
 
 class JobsWaitingResource extends Resource
 {
-    use TabsInResource;
+    use HasResourceTabs;
 
     protected static ?string $model = Job::class;
 
